030d27e9d2f568da03d12577f7e82af550396e0c
Mitchel Torres <mitchel.mrtp@gmail.com>
2024-10-15 22:07:10 -0500

    --

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; // Importa Provider
+import 'package:provider/provider.dart';
 import 'package:psicoapp/screens/Quest/quest_controller.dart';
 import 'screens/Auth/Login/login_page.dart';
 
